a68f3355478535d0e42203bba91fd417a0e20db9
Joshlion525 <78796082+Joshlion525@users.noreply.github.com>
2024-07-08 14:36:00 +0100

    ..

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,12 @@
 import React from "react";
+import { RxCross2 } from "react-icons/rx";
 
 const Modal = ({ card, closeModal }) => {
 	return (
 		<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-			<div className="bg-white p-10 rounded-lg w-3/4 max-w-md">
-				<div className="flex justify-end">
-					<button
-						className="px-4 py-2 bg-blue-500 text-white rounded"
-						onClick={closeModal}
-					>
-						Close
-					</button>
+			<div className="bg-white rounded-lg w-fit p-5 max-w-md">
+				<div className="flex justify-end pb-5">
+					<RxCross2 onClick={closeModal} className="text-xl cursor-pointer" />
 				</div>
 				<div className="flex justify-center">
 					<img src={card.title} alt="" />
